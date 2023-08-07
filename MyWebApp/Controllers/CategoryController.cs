e4c1efbed6c42404818bf1955c766b2e36fe5b65
mrvadhiya77 <mrvadhiya2002@gmail.com>
2023-08-07 22:29:31 +0530

    Added AddCategory, EditCategory, DleteCategory MVC action

--- MyWebApp/Controllers/CategoryController.cs
+++ MyWebApp/Controllers/CategoryController.cs
@@ -33,9 +33,69 @@ namespace MyWebApp.Controllers
             {
             _appContext.Categories.Add(category);
             _appContext.SaveChanges();
+                TempData["success"] = "Category Added Successfully !";
             return RedirectToAction("Index");
             }
             return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult EditCategory(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _appContext.Categories.Find(id);
+            if(category==null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _appContext.Categories.Update(category);
+                _appContext.SaveChanges();
+                TempData["success"] = "Category Edited Successfully !";
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult DeleteCategory(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _appContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("DeleteCategory")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id)
+        {
+            var category = _appContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _appContext.Categories.Remove(category);
+            _appContext.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully !";
+            return RedirectToAction("Index");
         }
     }
 }
