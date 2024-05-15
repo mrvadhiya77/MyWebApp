b114c08a6a8208cf78e5aa5ab990833a21a69358
mrvadhiya77 <mrvadhiya2002@gmail.com>
2024-05-15 22:50:32 +0530

    Add DataTable in product

--- MyWebApp/wwwroot/js/Product.js
+++ MyWebApp/wwwroot/js/Product.js
@@ -8,7 +8,15 @@ $(document).ready(function () {
                 { data: 'name' },
                 { data: 'description' },
                 { data: 'price' },
-                { data: 'category.name' }
+                { data: 'category.name' },
+                {
+                    data: "id",
+                    render: function (data) {
+                        return `
+           <a href = "/Admin/Product/AddEditProduct?id=${data}"><i class="bi bi-pencil-square"></i></a>
+                    <a asp-action="DeleteCategory" asp-controller="Category" asp-route-id="@item.Id"><i class="bi bi-trash-fill"></i></a>`;
+                    }
+                }
             ]
     });
 });
