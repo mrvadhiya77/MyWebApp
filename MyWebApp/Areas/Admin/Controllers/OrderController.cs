f42f4b7ac54ffeebfe82d4fc57671b22a38bfa7d
mrvadhiya77 <mrvadhiya2002@gmail.com>
2024-06-11 22:21:28 +0530

    Add Role Wise Order View

---
+++ MyWebApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApp.DataAccessLibrary.Infrastructure.IRepository;
+using MyWebApp.Models;
+using System.Security.Claims;
+
+namespace MyWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitWork;
+
+        public OrderController(IUnitOfWork unitWork)
+        {
+            _unitWork = unitWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Get Order Data
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult GetData()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+            if(User.IsInRole("Admin") || User.IsInRole("Employee"))
+            {
+                // All Data For Employee and Admin
+                orderHeaders = _unitWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                // Get Logged In User's Identity (Name, ID etc.)
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                // Get NameIdentifier From claimsIdentity
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                // To GetData For Perticular login user data
+                orderHeaders = _unitWork.OrderHeaders.GetAll(x => x.ApplicationUserId==claim.Value, includeProperties: "ApplicationUser");
+            }
+            return Json(new { data = orderHeaders });
+        }
+    }
+}
---
+++ MyWebApp/wwwroot/js/Order.js
@@ -0,0 +1,23 @@
+﻿var oTable;
+$(document).ready(function () {
+    oTable = $("#orderTable").DataTable({
+        ajax: {
+            url: "/Admin/Order/GetData"
+        },
+        columns: [
+            { data: 'name' },
+            { data: 'phone' },
+            { data: 'orderStatus' },
+            { data: 'orderTotal' },
+            {
+                data: "id",
+                render: function (data) {
+                    return `
+                        <a href = "/Admin/Order/AddEditOrder?id=${data}"><i class="bi bi-pencil-square"></i></a>`
+                    
+                }
+            }
+        ]
+    });
+
+});
