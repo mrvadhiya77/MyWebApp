4b16eae7dd290d24a6266d65e38bc57ca4f8f4de
mrvadhiya77 <mrvadhiya2002@gmail.com>
2024-06-18 23:15:25 +0530

    Prepare EmailSender Class For Email in CommanHelperRole

--- MyWebApp.CommonHelperRole/EmailSender.cs
+++ MyWebApp.CommonHelperRole/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace MyWebApp.CommonHelperRole
 {
@@ -7,6 +9,28 @@ namespace MyWebApp.CommonHelperRole
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var toEmail = new MimeMessage();
+            //From
+            toEmail.From.Add(MailboxAddress.Parse("MyEmail"));
+            //To
+            toEmail.To.Add(MailboxAddress.Parse(email));
+            //Subject
+            toEmail.Subject = subject;
+            //Body
+            toEmail.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            //Send Email
+            using(var emailClient = new SmtpClient())
+            {
+                //smtp.gmail.com-host, 587-port, MailKit.Security.SecureSocketOptions.StartTls - Security
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                // Set UserId and Password
+                emailClient.Authenticate("UserId", "Password");
+                emailClient.SendAsync(toEmail);
+                //Disconnect
+                emailClient.Disconnect(true);
+            }
+
             return Task.CompletedTask;
         }
     }
